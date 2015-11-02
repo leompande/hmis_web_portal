34f1d7ee417ffc5bacf84b5d7888b96d5e0f43cb
chubwa <tuzoengelbert@gmail.com>
2015-11-02 16:35:44 +0300

    Authentication burgs

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -16,7 +16,16 @@ angular.module("hmisPortal")
         });
     })
     .controller("mainCtrl",function ($rootScope,$scope,$http,$location,$timeout,olData,olHelpers,shared) {
-
+        jQuery(document).ready(function() {
+            $.post("https://dhis.moh.go.tz/dhis-web-commons-security/login.action?authOnly=true",
+                {withCredentials: true, params : {
+                    j_username: "portal", j_password: "Portal123"
+                }});
+            $.post("https://etl.moh.go.tz/dhis/dhis-web-commons-security/login.action?authOnly=true",
+                {withCredentials: true, params : {
+                    j_username: "portal", j_password: "Portal123"
+                }});
+        });
         $scope.cards = {};
         $scope.data = {};
         $rootScope.selectedOrgUnit = "m0frOspS7JY";
